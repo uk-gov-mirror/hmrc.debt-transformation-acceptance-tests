704a6fd5088c125ef36c3081bf7cc67f3d4288ab
thadeusssewanyana <sewadeus@hotmail.co.uk>
2021-03-08 15:41:35 +0000

    Feat : Added auth login to the endpoint tests

--- src/test/scala/uk/gov/hmrc/test/api/requests/BaseRequests.scala
+++ src/test/scala/uk/gov/hmrc/test/api/requests/BaseRequests.scala
@@ -7,31 +7,33 @@ import uk.gov.hmrc.test.api.utils.{BaseUris, RandomValues}
 trait BaseRequests extends RandomValues with BaseUris {
 
   def createBearerToken(
-    enrolments: Seq[String] = Seq(),
-    userType: String = getRandomAffinityGroup,
-    utr: String = "123456789012"
+      enrolments: Seq[String] = Seq(),
+      userType: String = getRandomAffinityGroup,
+      utr: String = "123456789012"
   ): String = {
     val json =
       Json.obj(
-        "affinityGroup"      -> userType,
+        "affinityGroup" -> userType,
         "credentialStrength" -> "strong",
-        "confidenceLevel"    -> 50,
-        "credId"             -> "test",
-        "enrolments"         -> enrolments.map(key =>
-          Json.obj(
-            "key"         -> key,
-            "identifiers" -> Json.arr(
-              Json.obj(
-                "key"   -> "UTRNumber",
-                "value" -> utr
-              )
-            ),
-            "state"       -> "Activated"
-          )
-        )
+        "confidenceLevel" -> 50,
+        "credId" -> "test",
+        "enrolments" -> enrolments.map(
+          key =>
+            Json.obj(
+              "key" -> key,
+              "identifiers" -> Json.arr(
+                Json.obj(
+                  "key" -> "UTRNumber",
+                  "value" -> utr
+                )
+              ),
+              "state" -> "Activated"
+          ))
       )
 
-    val response                          = WsClient.post(s"$authLoginApiUri/session/login", Map("Content-Type" -> "application/json"), json)
+    val response = WsClient.post(s"$authLoginApiUri/session/login",
+                                 Map("Content-Type" -> "application/json"),
+                                 json)
     val authHeader: (String, Seq[String]) = response.headers
       .filter(header => header._1.equalsIgnoreCase("Authorization"))
       .head
