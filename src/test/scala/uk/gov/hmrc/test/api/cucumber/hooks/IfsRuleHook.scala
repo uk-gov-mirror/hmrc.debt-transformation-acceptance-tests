155a17cd67359c93ea056ed8e4b8dce36d4bf51a
Kiran Pannala <kiranpl@users.noreply.github.com>
2021-04-14 15:38:43 +0100

    DTD-216: Workaround for the issue with rules collection

--- src/test/scala/uk/gov/hmrc/test/api/cucumber/hooks/IfsRuleHook.scala
+++ src/test/scala/uk/gov/hmrc/test/api/cucumber/hooks/IfsRuleHook.scala
@@ -3,11 +3,16 @@ package uk.gov.hmrc.test.api.cucumber.hooks
 import com.typesafe.scalalogging.LazyLogging
 import cucumber.api.scala.ScalaDsl
 import uk.gov.hmrc.test.api.utils.ScenarioContext
+import uk.gov.hmrc.test.api.requests.HelloWorldRequests
 
 class IfsRuleHook extends ScalaDsl with LazyLogging{
 
   Before() { _ =>
     ScenarioContext.reset()
+
+    //DTD-216: Workaround to load the rule set
+    val response = HelloWorldRequests.getInterestForecastingService("/hello-world")
+    Thread.sleep(2000)
   }
 
   After() { scenario =>
