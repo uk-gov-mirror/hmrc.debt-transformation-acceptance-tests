84916848ccae77673540605ac39e2fa38c67aba8
thadeusssewanyana <sewadeus@hotmail.co.uk>
2021-06-04 16:54:22 +0100

    Feat : refactored data file for sol

--- src/test/scala/uk/gov/hmrc/test/api/cucumber/stepdefs/sol/StatementOfLiabilitySteps.scala
+++ src/test/scala/uk/gov/hmrc/test/api/cucumber/stepdefs/sol/StatementOfLiabilitySteps.scala
@@ -97,17 +97,6 @@ class StatementOfLiabilitySteps extends ScalaDsl with EN with Eventually with Ma
     TestData.loadedFiles(variant)
 
   And("""add debt item chargeIDs to the debt""") { (dataTable: DataTable) =>
-    /*val asMapTransposed = dataTable.asMaps(classOf[String], classOf[String])
-    var dutyChargeIds   = ""
-
-    asMapTransposed.zipWithIndex.foreach { case (dutyId, index) =>
-      dutyChargeIds = dutyChargeIds.concat(dutyId.get("dutyId"))
-      if (index + 1 < asMapTransposed.size) dutyChargeIds = dutyChargeIds.concat(",")
-    }
-    val jsonWithDutyChargeId =
-      ScenarioContext.get("debtDetails").toString.replaceAll("<REPLACE_debtItemChargeIDs>", dutyChargeIds)
-    ScenarioContext.set("debtDetails", jsonWithDutyChargeId)
-    print(" ******************************* " + jsonWithDutyChargeId)*/
     StatementOfLiabilityRequests.addDutyIds(dataTable)
   }
 
