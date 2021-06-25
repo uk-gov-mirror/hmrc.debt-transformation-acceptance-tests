d8e9f01cb72961c703309c09f9b206d5240a06df
Rocco Bruno <rocco_bruno@msn.com>
2021-06-25 11:54:43 +0100

    DTD-425: updated suppression APIs

--- src/test/scala/uk/gov/hmrc/test/api/requests/SuppressionRulesRequests.scala
+++ src/test/scala/uk/gov/hmrc/test/api/requests/SuppressionRulesRequests.scala
@@ -30,7 +30,7 @@ object SuppressionRulesRequests extends ScalaDsl with EN with Eventually with Ma
 
   def postSuppressionData(json: String, id: String): StandaloneWSResponse = {
     val bearerToken = createBearerToken(enrolments = Seq("read:suppression-data"))
-    val baseUri     = s"$interestForecostingApiUrl/suppressions/$id/suppression"
+    val baseUri     = s"$interestForecostingApiUrl/suppressions/$id"
     val headers     = Map(
       "Authorization" -> s"Bearer $bearerToken",
       "Content-Type"  -> "application/json",
@@ -54,7 +54,7 @@ object SuppressionRulesRequests extends ScalaDsl with EN with Eventually with Ma
 
   def postSuppressionRules(json: String, rulesID: String): StandaloneWSResponse = {
     val bearerToken = createBearerToken(enrolments = Seq("read:suppression-rule"))
-    val baseUri     = s"$interestForecostingApiUrl/suppression-rules/$rulesID/suppression-rule"
+    val baseUri     = s"$interestForecostingApiUrl/suppression-rules"
     val headers     = Map(
       "Authorization" -> s"Bearer $bearerToken",
       "Content-Type"  -> "application/json",
@@ -164,6 +164,7 @@ object SuppressionRulesRequests extends ScalaDsl with EN with Eventually with Ma
     println(s"SUPPRESSION RULES REQUEST --> $request")
 
     val response = SuppressionRulesRequests.postSuppressionRules(request, rulesID)
+    println(response.body)
     response.status should be(200)
   }
 }
