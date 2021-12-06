a7d80ee3ad1fa67cfd1649584d505a474002d4f5
vasanthavijaya <vijaya.vasantha@digital.hmrc.gov.uk>
2021-12-06 23:36:36 +0000

    adding tests to new fc VAT end point

---
+++ src/test/scala/uk/gov/hmrc/test/api/models/FCVATDebtCalculationsSummary.scala
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ */
+
+package uk.gov.hmrc.test.api.models
+
+import java.time.LocalDate
+
+import play.api.libs.json.{Json, OFormat}
+
+case class FCVATDebtCalculationsSummary(
+                                      dateOfCalculation: Option[LocalDate],
+                                      combinedDailyAccrual: Int,
+                                      unpaidAmountTotal: Int,
+                                      debtCalculations: List[FCVATDebtCalculation]
+                                    )
+
+
+object FCVATDebtCalculationsSummary {
+  implicit val formatDebtCalculation: OFormat[FCVATDebtCalculationsSummary] = Json.format[FCVATDebtCalculationsSummary]
+}
