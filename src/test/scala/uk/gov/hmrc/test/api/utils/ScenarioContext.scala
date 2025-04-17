a5724db0b5052ee5854b41d155abcc25b8c2d7ae
thadeusssewanyana <sewadeus@hotmail.co.uk>
2025-04-17 13:26:31 +0100

    Feat Added scenario to cover all suppression criteria for a single debt item

--- src/test/scala/uk/gov/hmrc/test/api/utils/ScenarioContext.scala
+++ src/test/scala/uk/gov/hmrc/test/api/utils/ScenarioContext.scala
@@ -16,40 +16,27 @@
 
 package uk.gov.hmrc.test.api.utils
 
-import uk.gov.hmrc.test.api.models.{Payment, SuppressionInformation}
+import uk.gov.hmrc.test.api.models.SuppressionInformation
 
 object ScenarioContext {
   private var scenarioValues = Map.empty[String, Any]
   private var suppression    = Map.empty[String, List[SuppressionInformation]]
-  private var paymentHistory = Map.empty[String, List[Payment]]
 
   def set(key: String, value: Any): Unit                                     =
     scenarioValues = scenarioValues + (key -> value)
 
   def setSuppression(key: String, value: List[SuppressionInformation]): Unit =
     suppression = suppression + (key -> value)
-
-  def getSuppression[T: Manifest](key: String): T                            =
-    suppression
-      .get(key)
-      .fold(throw new Exception(s"Key $key not found in scenario context"))(_.asInstanceOf[T])
   def get[T: Manifest](key: String): T                                       =
     scenarioValues
       .get(key)
       .fold(throw new Exception(s"Key $key not found in scenario context"))(_.asInstanceOf[T])
 
   def remove(key: String): Unit = scenarioValues = scenarioValues - key
-
-  def setPaymentHistory(key: String, value: List[Payment]): Unit =
-    paymentHistory = paymentHistory + (key -> value)
-
-  def getPaymentHistory[T: Manifest](key: String): T =
-    paymentHistory.get(key).fold(throw new Exception(s"Key $key not found in scenario context"))(_.asInstanceOf[T])
-
   def reset(): Unit = {
     scenarioValues = Map.empty[String, Any]
-    suppression = Map.empty[String, List[SuppressionInformation]]
-    paymentHistory = Map.empty[String, List[Payment]]
+
+
 
   }
 }
